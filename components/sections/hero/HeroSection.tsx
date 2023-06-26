46df3859db7e7690f5360e1009495c491e1b34af
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-06-26 12:03:49 +0300

    edited: hero section module styles

--- components/sections/hero/HeroSection.tsx
+++ components/sections/hero/HeroSection.tsx
@@ -1,9 +1,9 @@
 import { SwiperBlock } from "@/components/swiper/SwiperBlock";
 import React from "react";
 
-import "./heroSection.scss";
+import styles from "./HeroSection.module.scss";
 export const HeroSection: React.FC = () => (
-  <section className="heroSection">
+  <section className={styles.heroSection}>
     <SwiperBlock />
   </section>
 );
